743939803a2f0a5c84b3b47c932e48fc9b2162c1
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-09-04 19:05:39 +0900

    add axios

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ var express = require('express');
 var app = module.exports = express();
 var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
+const cors = require('cors');
+
 app.set('trust proxy', true);
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(require('cookie-parser')());
+app.use(cors());
 
-app.get('/', function(req, res) {
-  console.log('GET /');
-  res.send('<h1>foo lala</h1>');
-})
 
 var port = 1987;
 var ipaddr = '127.0.0.1';
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
 server.listen(port, ipaddr, function() {
   console.log('Express server listening on port ' + server.address().port + ' at ' + server.address().address);
 });
+
+app.get('/', cors(corsOptions), function(req, res) {
+  console.log('GET /');
+  res.send('<h1>foo lala</h1>');
+})
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -10,19 +11,28 @@ class App extends Component {
     };
   }
 
-  handleClickButton(){
-
-    this.setState({list: [{name:"kyeongrok"}]});
+  handleClickButton() {
+    axios.get('http://localhost:1987')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    this.setState({list: [{name: "kyeongrok"}]});
   }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-            <button onClick={()=>this.handleClickButton()}>digitec</button>
+          <button onClick={() => this.handleClickButton()}>digitec</button>
         </div>
         <p className="App-intro">
           Hello Electron!
+
         </p>
+        {this.state.list.map(item => <p key={item.name}>{item.name}</p>)}
       </div>
     );
   }
