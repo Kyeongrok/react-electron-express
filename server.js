94973c2ac310c387baea76f5ac2db73a217f5d75
kyeongrok <oceanfog1@gmail.com>
2017-09-05 01:35:34 +0900

    eslint 적용

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-#!/usr/bin/env node
-var express = require('express');
-var app = module.exports = express();
-var server = require('http').createServer(app);
-var bodyParser = require('body-parser');
+// eslint-disable-next-line import/newline-after-import
+const express = require('express');
+const app = module.exports = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.set('trust proxy', true);
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(require('cookie-parser')());
 app.use(cors());
 
 
-var port = 1987;
-var ipaddr = '127.0.0.1';
+const port = 1987;
+const ipaddr = '127.0.0.1';
 
-var corsOptions = {
+const corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-
-server.listen(port, ipaddr, function() {
-  console.log('Express server listening on port ' + server.address().port + ' at ' + server.address().address);
+server.listen(port, ipaddr, () => {
+  console.log(`Express server listening on port ${server.address().port} at ${server.address().address}`);
 });
 
-app.get('/', cors(corsOptions), function(req, res) {
+app.get('/', cors(corsOptions), (req, res) => {
   console.log('GET /');
   res.send('<h1>foo lala</h1>');
-})
+});
