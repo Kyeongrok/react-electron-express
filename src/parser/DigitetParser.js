11165b4a813e8cde6aff079a558967e279fb652f
kyeongrok <oceanfog1@gmail.com>
2017-09-04 22:57:14 +0900

    add parser

---
+++ src/parser/DigitetParser.js
@@ -0,0 +1,22 @@
+const client = require('cheerio-httpcli');
+
+const printHttpResponse = url => client.fetch(url, {}, (err, $) => {
+  if (err) { console.log('error:', err); return; }
+
+  const list = $('.addProductTo');
+
+  for (let i = 0; i < list.length; i++) {
+
+  }
+
+  const string = list[1].attribs.onclick;
+
+
+  console.log(string);
+  // const json = JSON.parse(string.replace('dataLayer.push(', '').replace(');', ''));
+  // const match = string.match(/data/m);
+  // console.log(match.length);
+});
+const url = 'https://www.digitec.ch/de/s1/producttype/tv-4?tagIds=538&take=10';
+printHttpResponse(url);
+
