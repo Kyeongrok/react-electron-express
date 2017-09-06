4d2b55aaead474c27e60187cd617a7fa01299d99
kyeongrok <oceanfog1@gmail.com>
2017-09-07 04:26:31 +0900

    필요 없는 기호 정리

--- src/parser/DigitecParser.js
+++ src/parser/DigitecParser.js
@@ -1,11 +1,15 @@
 const getProductInfo = (productContent) => {
   const productInfo = {};
-  productInfo.currency = productContent.children[1].children[1].children[0].data;
-  productInfo.price = productContent.children[1].children[2].data;
-  productInfo.name = productContent.children[3].children[2].data;
+  const currency = productContent.children[1].children[1].children[0].data;
+  const price = productContent.children[1].children[2].data;
+  const name = productContent.children[3].children[2].data;
+  productInfo.currency = currency;
+  productInfo.price = price.replace(/[ –\\.]/g, '');
+  productInfo.name = name.replace(/[\r\n]/g, '');
 
   if (productContent.children[1].children.length === 4) {
-    productInfo.appendix = productContent.children[1].children[3].children[0].data;
+    const appendix = productContent.children[1].children[3].children[0].data;
+    productInfo.appendix = appendix.replace(/[(statt vorher)–\\.]/g, '');
   }
   return productInfo;
 };
