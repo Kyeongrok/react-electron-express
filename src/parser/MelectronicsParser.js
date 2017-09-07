da8330dc5c4d09c987dfe61dafa974712079dad9
yeongseon <yeongseon.choe@gmail.com>
2017-09-08 00:33:00 +0900

    Add item url

--- src/parser/MelectronicsParser.js
+++ src/parser/MelectronicsParser.js
@@ -1,15 +1,16 @@
-const melectronicsUrl = 'http://www.melectronics.ch/c/de/TV_%26_Audio/Fernseher/';
+const melectronics_url = 'http://www.melectronics.ch/c/de/TV_%26_Audio/Fernseher/';
 
+const item_url = 'http://www.melectronics.ch/p/de/LG_49SJ810V_123_cm_4K_Fernseher/770337600000/?listIndex=%d&service=false'
 const request = require('request');
 const cheerio = require('cheerio');
 
-const requestOptions = {
+const request_options = {
   method: 'GET',
-  uri: melectronicsUrl,
+  uri: melectronics_url,
   headers: { 'User-Agent': 'Mozilla/5.0' },
 };
 
-request(requestOptions, (error, response, html) => {
+request(request_options, (error, response, html) => {
   if (error) { throw error; }
   // console.log(html);
 
@@ -18,7 +19,11 @@ request(requestOptions, (error, response, html) => {
 
   // find total number of item
   $('.total').first().each(function (index, elem) {
-    console.log($(this).text())
+    // console.log($(this).text())
+    var item_number = Number($(this).text())
+
+    
   })
+
 
 });
